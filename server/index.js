5ed3e0966b6ba26242c414d26eee78b44d9ef705
Rohan Barsagade <rbarsagade101@gmail.com>
2023-10-18 12:20:58 +0530

    add environment variable examples

--- server/index.js
+++ server/index.js
@@ -2,16 +2,19 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const updateData = require("./utilities/cronjob");
+
+const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
 app.use(
   cors({
-    origin: "*",
+    origin: [FRONTEND_URL],
   })
 );
+
 const cron = require("node-cron");
 const router = require("./routes/routes");
 const addSubscription = require("./controllers/addSubscription");
-
-const PORT = 5000;
 
 app.use(express.static("data"));
 app.use(express.json());
